c01c0326a1f444a4679eb9d3d2eb1747b0fe4636
Soumya S <soumyas@live.com>
2017-02-28 15:58:02 -0800

    Initial commit

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, \
+    redirect, url_for, jsonify, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
@@ -16,16 +17,19 @@ app = Flask(__name__)
 # Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-client_ID = '290206841222-r6nlpufcqg6supd67h4quuj2ekfqn4i7.apps.googleusercontent.com'
 secret_key = 'VrdKYIFXxZ66u-YAJ2yit90M'
 CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+   open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Item Catalog"
 
+# Make login_session available to templates
+app.jinja_env.globals['login_session'] = login_session
+
+
+# Displays the catalog landing/home page
 @app.route('/')
 @app.route('/catalog')
 def get_catalog():
@@ -37,27 +41,33 @@ def get_catalog():
                            items=latest_items)
 
 
+# A JSON end point that serves the details of the catalog
 @app.route('/catalog/json/')
 def get_catalog_json():
     categories = session.query(Category).all()
     return jsonify(Categories=[c.serialize for c in categories])
 
 
+# Displays the category details page
 @app.route('/category/<int:category_id>/')
 def get_category(category_id):
     categories = session.query(Category).all()
-    category = session.query(Category).filter(Category.id == category_id).first()
+    category = session.query(Category).filter(
+                            Category.id == category_id).first()
     return render_template('category.html',
                            categories=categories,
                            category=category)
 
 
+# A JSON end point that serves the details of a category
 @app.route('/category/<int:category_id>/json')
 def get_category_json(category_id):
-    category = session.query(Category).filter(Category.id == category_id).first()
+    category = session.query(Category).filter(
+        Category.id == category_id).first()
     return jsonify(Category=[category.serialize])
 
 
+# Displays the Items details page
 @app.route('/category/<int:category_id>/<int:item_id>/')
 def get_item(category_id, item_id):
     item = session.query(Item).filter(Item.item_id == item_id,
@@ -66,12 +76,14 @@ def get_item(category_id, item_id):
                            item=item)
 
 
+# A JSON end point that serves the details of an item in the catalog
 @app.route('/item/<int:item_id>/json')
 def get_item_json(item_id):
     item = session.query(Item).filter(Item.item_id == item_id).first()
     return jsonify(Item=[item.serialize])
 
 
+# Allows the user to update the details of an item in the catalog
 @app.route('/item/<int:item_id>/update',
            methods=['GET', 'POST'])
 def update_item(item_id):
@@ -79,30 +91,49 @@ def update_item(item_id):
     item = session.query(Item).filter(Item.item_id == item_id).first()
 
     if request.method == 'GET':
+        if 'user_id' not in login_session:
+            flash('Please log in to update an item')
         return render_template('edititem.html', item=item,
                                categories=categories)
     else:
-        name = request.form['name']
-        description = request.form['description']
-        category = request.form['category']
-        user_id = 1
-        item.name = name
-        item.description = description
-        item.category_id = category
-        item.user_id = user_id
-        session.commit()
+        if 'user_id' not in login_session:
+            flash('Please log in to update an item')
+        elif item.user_id != login_session['user_id']:
+            flash('You do not have permission to edit the item details')
+        else:
+            name = request.form['name']
+            description = request.form['description']
+            category = request.form['category']
+            user_id = login_session['user_id']
+            if not (name and description and category and user_id):
+                flash('One or more fields are blank')
+            else:
+                item.name = name
+                item.description = description
+                item.category_id = category
+                item.user_id = user_id
+                session.commit()
+                return redirect(url_for('get_catalog'))
 
-        return redirect(url_for('get_catalog'))
+        return redirect(url_for('update_item', item_id=item_id))
 
 
+# Allows a user to delete an item
 @app.route('/item/<int:item_id>/delete')
 def delete_item(item_id):
     item = session.query(Item).filter(Item.item_id == item_id).first()
-    session.delete(item)
-    session.commit()
-    return redirect(url_for('get_catalog'))
+    if 'user_id' not in login_session:
+        flash('Please log in to delete an item')
+    elif item.user_id != login_session['user_id']:
+        flash('You do not have permission to delete the item')
+    else:
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('get_catalog'))
+    return redirect(url_for('update_item', item_id=item_id))
 
 
+# Allows a user to add an item
 @app.route('/item/new', methods=['GET', 'POST'])
 def add_item():
 
@@ -110,18 +141,31 @@ def add_item():
         categories = session.query(Category).all()
         return render_template('newitem.html', categories=categories)
     else:
-        name = request.form['name']
-        description = request.form['description']
-        category = request.form['category']
-        user_id = 1
-        item = Item(name = name,
-                    description = description,
-                    category_id = category,
-                    user_id = user_id)
-        session.add(item)
-        session.commit()
+        error = None
+        if 'user_id' not in login_session:
+            error = 'Please log in to add an item'
+        else:
+            name = request.form['name']
+            description = request.form['description']
+            category = request.form['category']
+            user_id = login_session['user_id']
+            if not (name and description and category and user_id):
+                error = 'One or more fields are blank'
+            else:
+                item = Item(name=name,
+                            description=description,
+                            category_id=category,
+                            user_id=user_id)
+                session.add(item)
+                session.commit()
 
-        return redirect(url_for('get_catalog'))
+        categories = session.query(Category).all()
+        latest_items = session.query(Item).order_by(
+            Item.created_on.desc()).limit(5).all()
+        # return redirect(url_for('get_catalog', error=error))
+        return render_template('cataloglanding.html', error=error,
+                               categories=categories,
+                               items=latest_items)
 
 
 # Create anti-forgery state token
@@ -130,10 +174,108 @@ def login():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
 
+# User sign-in and authentication using Google api
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = \
+            make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v3/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    # Check if the user has already been added to the db.
+    # If not, add user details to the DB
+    user = session.query(User).filter(
+        User.email == login_session['email']).first()
+    if not user:
+        new_user = User(name=login_session['username'],
+                        email=login_session['email'])
+        session.add(new_user)
+        session.commit()
+        login_session['user_id'] = new_user.id
+    else:
+        login_session['user_id'] = user.id
+    return 'Welcome'
+
+
+# This is for logging out a user
+@app.route('/gdisconnect')
+def gdisconnect():
+    login_session.clear()
+    flash('Log out successful')
+    return redirect(url_for('get_catalog'))
+
+
+# Application details
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
